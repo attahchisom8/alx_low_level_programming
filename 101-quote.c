5783715d68742e7bd09547d906dabcb77aee28ca
attahchisom8 <attahchisom8@gmail.com>
2025-12-17 23:49:50 +0100

    c

---
+++ 101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - program execution point
+ * @
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
