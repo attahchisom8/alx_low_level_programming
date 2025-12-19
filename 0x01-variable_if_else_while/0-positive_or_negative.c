4eb71d8f541585ecb723df633122ed4255503223
attahchisom8 <attahchisom8@gmail.com>
2025-12-19 17:09:18 +0100

    variabe if else while

---
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+
+/**
+ * main - function main entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(NULL));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
